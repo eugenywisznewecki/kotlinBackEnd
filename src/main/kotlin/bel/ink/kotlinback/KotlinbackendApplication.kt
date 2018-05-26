640816a36b92ac645b9b9625ac734b6ac0aa4546
Eugeny Yu <eugeny.yushkevich@gmail.com>
2018-05-26 23:55:59 +0300

    initial

---
+++ src/main/kotlin/bel/ink/kotlinback/KotlinbackendApplication.kt
@@ -0,0 +1,11 @@
+package bel.ink.kotlinback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinbackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinbackendApplication>(*args)
+}
